560999ba80353804dd17741929b71a36d2eddd27
Saud Naif <saudroyalpc@gmail.com>
2024-03-11 18:13:57 +0300

    adding seeders for users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Group;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,16 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesAndPermissionsSeeder::class);
         $this->call(DepartmentSeeder::class);
-        User::factory(10)->create();
+        $this->call(UserSeeder::class);
         Group::factory(100)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Saud',
-        //     'email' => 'admin@example.com',
-        //     'university_id' => 442003532,
-        //     'department' => 'Computer Science',
-        //     'state' => StudentStates::NotJoined,
-        //     'password' => bcrypt('111'),
-        // ]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Base\RolesList;
+use App\Models\Department;
+use App\Models\User;
+use App\States\StudentStates;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        User::factory(15)->create();
+
+        /** @var User $student */
+        $student1 = User::create([
+            'name' => 'سعود العتيبي',
+            'email' => 'student@example.com',
+            'university_id' => 442003532,
+            'department_id' => Department::query()->get()->random()->id,
+            'state' => StudentStates::NotJoined,
+            'password' => Hash::make(111),
+        ]);
+        $student1->assignRole(RolesList::ROLE_STUDENT);
+
+        /** @var User $student */
+        $student2 = User::create([
+            'name' => 'محمد الشريف',
+            'email' => 'student2@example.com',
+            'university_id' => 443003532,
+            'department_id' => Department::query()->get()->random()->id,
+            'state' => StudentStates::NotJoined,
+            'password' => Hash::make(111),
+        ]);
+        $student2->assignRole(RolesList::ROLE_STUDENT);
+
+        /** @var User $student */
+        $student3 = User::create([
+            'name' => 'معيض الزهراني',
+            'email' => 'student3@example.com',
+            'university_id' => 444003532,
+            'department_id' => Department::query()->get()->random()->id,
+            'state' => StudentStates::NotJoined,
+            'password' => Hash::make(111),
+        ]);
+        $student3->assignRole(RolesList::ROLE_STUDENT);
+
+        /** @var User $student */
+        $student4 = User::create([
+            'name' => 'محمد احمد',
+            'email' => 'student4@example.com',
+            'university_id' => 445303532,
+            'department_id' => Department::query()->get()->random()->id,
+            'state' => StudentStates::NotJoined,
+            'password' => Hash::make(111),
+        ]);
+        $student4->assignRole(RolesList::ROLE_STUDENT);
+    }
+}
