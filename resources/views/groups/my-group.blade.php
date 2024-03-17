2888dd495d19ef22e38471683b6331ef2c626663
Saud Naif <saudroyalpc@gmail.com>
2024-03-18 01:09:08 +0300

    modifying

--- resources/views/groups/my-group.blade.php
+++ resources/views/groups/my-group.blade.php
@@ -12,13 +12,13 @@
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $group->name }}
             @if ($group->status == 'new')
                 <span
-                    class="bg-gray-100 text-gray-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300">{{ $group->status }}</span>
+                    class="bg-gray-100 text-gray-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300">New</span>
             @elseif ($group->status == 'pending')
                 <span
-                    class="bg-yellow-100 text-yellow-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300">{{ $group->status }}</span>
+                    class="bg-yellow-100 text-yellow-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300">Pending</span>
             @elseif ($group->status == 'confirmed')
                 <span
-                    class="bg-green-100 text-green-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">{{ $group->status }}</span>
+                    class="bg-green-100 text-green-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">Confirmed</span>
             @endif
         </h5>
         {{-- Displaying Group Leader --}}
