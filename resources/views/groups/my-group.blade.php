615cdf9eb6623a26d0b8ae48cdedc2901a7ca5c9
MuidhAlzahrani <xmeed43@gmail.com>
2024-03-17 21:02:03 +0300

    Adding Dark mode to the profile drop down menu and fixing group card informations

--- resources/views/groups/my-group.blade.php
+++ resources/views/groups/my-group.blade.php
@@ -5,11 +5,35 @@
         $users = App\Models\User::where('group_id', $group->id)->get();
     }
 @endphp
-<div class="flex items-center justify-center h-64">
-    <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$group->name}}</h5>
+<div class="flex items-center justify-center p-6">
+    <div
+        class="max-w-sm p-6 min-w-96 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        {{-- Displaying Group name and status --}}
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $group->name }}
+            @if ($group->status == 'new')
+                <span
+                    class="bg-gray-100 text-gray-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300">{{ $group->status }}</span>
+            @elseif ($group->status == 'pending')
+                <span
+                    class="bg-yellow-100 text-yellow-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300">{{ $group->status }}</span>
+            @elseif ($group->status == 'confirmed')
+                <span
+                    class="bg-green-100 text-green-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">{{ $group->status }}</span>
+            @endif
+        </h5>
+        {{-- Displaying Group Leader --}}
         @foreach ($users as $user)
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$user->name}}&nbsp;{{$user->state}}</p>
+            @if ($user->state == 'group_leader')
+                <p class="mb-3 font-bold text-gray-700 dark:text-gray-400">Group Leader <span class="font-normal">{{ $user->name }}</span> </p>
+            @endif
+        @endforeach
+        <hr class="p-1">
+        {{-- Displaying Group Members --}}
+        <p class="mb-3 font-bold text-gray-700 dark:text-gray-400">Group Members</p>
+        @foreach ($users as $user)
+            @if ($user->state == 'group_member')
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $user->name }}</p>
+            @endif
         @endforeach
     </div>
 </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,9 +20,6 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <a href="{{ route('toggle-dark-mode') }}" class="text-blue-500">
-                    Toggle Dark Mode
-                </a>
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
@@ -49,6 +46,15 @@
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        {{-- Dark Mode Switch --}}
+                            <hr class="m-1 dark:border-gray-500">
+                            <x-dropdown-link :href="route('toggle-dark-mode')">
+                                @if(session('darkMode'))
+                                    {{__('Light Mode')}}
+                                @else
+                                    {{__('Dark Mode')}}
+                                @endif
                             </x-dropdown-link>
                         </form>
                     </x-slot>
@@ -95,6 +101,15 @@
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
+                    </x-responsive-nav-link>
+                {{-- Responsive Dark Mode Switch --}}
+                    <hr class="m-1 dark:border-gray-500">
+                    <x-responsive-nav-link :href="route('toggle-dark-mode')">
+                        @if(session('darkMode'))
+                            {{__('Light Mode')}}
+                        @else
+                            {{__('Dark Mode')}}
+                        @endif
                     </x-responsive-nav-link>
                 </form>
             </div>
