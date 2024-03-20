f3a7f96858d24b50361a34db892b3f2f0545969f
MuidhAlzahrani <xmeed43@gmail.com>
2024-03-20 05:36:25 +0300

    fixing profile info card

--- resources/views/profile/my-profile.blade.php
+++ resources/views/profile/my-profile.blade.php
@@ -4,11 +4,11 @@
     $group = App\Models\Group::find($user->group_id);
 @endphp
 <div class="flex items-center justify-center p-6">
-    <div class="grid lg:grid-cols-3 md:grid-cols-1 w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">  
+    <div class="grid lg:grid-cols-3 md:grid-cols-1 w-[80rem] max-w-7xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">  
         {{-- Displaying Student name --}}
         <x-user-info label="Name">{{ $user->name }}</x-user-info>
         {{-- Displaying ID --}}
-        <x-user-info label="University ID">{{ $user->university_id }}</x-user-info>
+        <x-user-info label="Student ID">{{ $user->university_id }}</x-user-info>
         {{-- Displaying E-mail --}}
         <x-user-info label="Email">{{ $user->email }}</x-user-info>
         {{-- Displaying Department --}}
