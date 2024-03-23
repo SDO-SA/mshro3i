9189ebd3ae4050dc2c77ab4e97ed8bd881574d62
Saud Naif <saudroyalpc@gmail.com>
2024-03-24 01:21:57 +0300

    language setup

---
+++ lang/en/app.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'login' => 'Log in',
+    'register' => 'Register',
+    'already_registerd' => 'Already Registerd ?',
+    'forgot_password' => 'Forgot your password?',
+    'remember_me' => 'Remember me',
+    'email' => 'Email',
+    'password' => 'Password',
+    'name' => 'Name',
+    'enter_full_name' => 'Enter Your Full Name',
+];
