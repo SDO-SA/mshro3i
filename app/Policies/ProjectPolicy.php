477b22be724836655f50a9472f617f91ab7dec61
MuidhAlzahrani <xmeed43@gmail.com>
2024-03-26 01:33:58 +0300

    fixed create project dont disappear after creation bug

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -5,6 +5,7 @@ namespace App\Policies;
 use App\Base\RolesList;
 use App\Models\User;
 use App\States\StudentStates;
+use App\Models\Project;
 
 class ProjectPolicy
 {
@@ -13,6 +14,6 @@ class ProjectPolicy
      */
     public function canCreateProjectProposal(User $user): bool
     {
-        return $user->hasRole(RolesList::ROLE_STUDENT) && $user->state === StudentStates::GroupLeader()->value;
+        return $user->hasRole(RolesList::ROLE_STUDENT) && $user->state === StudentStates::GroupLeader()->value && ! Project::where('group_id', $user->group_id)->exists();
     }
 }
