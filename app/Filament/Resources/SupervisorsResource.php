9f29682a2e34647b54662c1399a1ae84a68743e0
Saud Al-Otaibi <saudroyalpc@gmail.com>
2024-03-31 04:20:28 +0300

    fix

--- app/Filament/Resources/SupervisorsResource.php
+++ app/Filament/Resources/SupervisorsResource.php
@@ -19,7 +19,7 @@ class SupervisorsResource extends Resource
 {
     protected static ?string $model = Supervisor::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
 
     public static function form(Form $form): Form
     {
