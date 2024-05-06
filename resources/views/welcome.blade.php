381cdd884517d0cef79cc2ba3f2279993dd90f3e
Saud Naif <saudroyalpc@gmail.com>
2024-05-06 17:42:31 +0300

    refactor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Fin</title>
+        <title>مشروعي</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
