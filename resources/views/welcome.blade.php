faf90d56b983318a332260477b572a1c54970d03
MuidhAlzahrani <xmeed43@gmail.com>
2024-05-10 00:26:28 +0300

    fixed colors agane

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
                       <p class="mt-6 text-lg leading-8 text-gray-600">تنظيم وادارة مشروع تخرجك صار ولا أسهل باستخدام منصتنا</p>
                       <div class="mt-10 flex items-center justify-center gap-x-6">
                         <a href="{{ route('login') }}" class="text-sm font-semibold leading-6 text-gray-900">سجل دخول</a>
-                        <a href="{{ route('register') }}" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">سجل معانا <span aria-hidden="true">→</span></a>
+                        <a href="{{ route('register') }}" class="rounded-md bg-royalblue-100 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-royalblue-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-royalblue-100">سجل معانا <span aria-hidden="true">→</span></a>
                       </div>
                     </div>
                 </div>
